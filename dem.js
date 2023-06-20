e67f890c0bf0eeb0d2641b653ad15dd9bf433a87
vkpvivek <vkpvivek67@gmail.com>
2023-06-20 12:56:59 +0530

    my fourth comit

--- dem.js
+++ dem.js
@@ -28,12 +28,24 @@
 // console.log(items);
 
     //Get Element by ClassName
-    
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[2].style.backgroundColor="LightGreen";
 
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight="bold";
+// var items=document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[2].style.backgroundColor="LightGreen";
+
+// for(var i=0;i<items.length;i++){
+//     items[i].style.fontWeight="bold";
+// }
+
+    //Get Element by TagName
+
+var li=document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[2].style.backgroundColor="LightGreen";
+
+
+for(var i=0;i<li.length;i++){
+    li[i].style.fontWeight="bold";
 }
